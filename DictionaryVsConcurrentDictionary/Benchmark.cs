9b6fc3bf3d6601c9db6c6a87289661f6e33c8624
PhiseySt <phstas2016@rambler.ru>
2022-11-10 10:19:52 +0300

    Add dictionary vs concurrentdictionary benchmark

---
+++ DictionaryVsConcurrentDictionary/Benchmark.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace DictionaryVsConcurrentDictionary;
+
+[MemoryDiagnoser]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn, MeanColumn, MedianColumn]
+[HideColumns("Gen0", "Gen1", "Gen2")]
+public class Benchmark
+{
+    private readonly int _numberAddItems = 10000;
+    private readonly string[] _keys;
+    private readonly int[] _values;
+    private readonly Random _rng = new();
+    private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public Benchmark()
+    {
+        _keys = new string[_numberAddItems];
+        _values = new int[_numberAddItems];
+
+        for (var x = 0; x < _keys.Length; x++)
+        {
+            if (x % 5 != 0)
+            {
+                _keys[x] = RandomString((x % 5) + 1);
+                _values[x] = x;
+            }
+            else
+            {
+                _keys[x] = "Hello World";
+                _values[x] = 555;
+            }
+        }
+    }
+
+    private string RandomString(int size)
+    {
+        var buffer = new char[size];
+
+        for (var i = 0; i < size; i++)
+        {
+            buffer[i] = Chars[_rng.Next(Chars.Length)];
+        }
+        return new string(buffer);
+    }
+
+    [Benchmark]
+    public void NotParallelAddToConcurrent() => Methods.NotParallelAddToConcurrent(_numberAddItems, _keys, _values);
+
+    [Benchmark]
+    public void NotParallelAddToDictionary() => Methods.NotParallelAddToDictionary(_numberAddItems, _keys, _values);
+
+    [Benchmark]
+    public void ParallelAddToConcurrent() => Methods.ParallelAddToConcurrent(_numberAddItems, _keys, _values);
+
+    [Benchmark]
+    public void ParallelAddToDictionary() => Methods.ParallelAddToDictionary(_numberAddItems, _keys, _values);
+
+}
---
+++ DictionaryVsConcurrentDictionary/Methods.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Concurrent;
+
+namespace DictionaryVsConcurrentDictionary;
+
+public static class Methods
+{
+    public static void NotParallelAddToDictionary(int numberAddItems, string[] k, int[] v)
+    {
+        Dictionary<string, int> d = new();
+
+        for (var i = 0; i < numberAddItems; i++)
+        {
+            SomeLongRunningMethod();
+
+            if (!d.ContainsKey(k[i]))
+                d.Add(k[i], v[i]);
+            else
+                d[k[i]] = v[i];
+        }
+    }
+
+    public static void ParallelAddToDictionary(int numberAddItems, string[] k, int[] v)
+    {
+        object lockObject = new();
+        Dictionary<string, int> d = new();
+
+        Parallel.For(0, numberAddItems, x =>
+        {
+            SomeLongRunningMethod();
+            lock (lockObject)
+            {
+                if (!d.ContainsKey(k[x]))
+                    d.Add(k[x], v[x]);
+                else
+                    d[k[x]] = v[x];
+            }
+        });
+    }
+
+    public static void NotParallelAddToConcurrent(int numberAddItems, string[] k, int[] v)
+    {
+        ConcurrentDictionary<string, int> cd = new();
+
+        for (var i = 0; i < numberAddItems; i++)
+        {
+            SomeLongRunningMethod();
+            cd.AddOrUpdate(k[i], v[i], (k, v) => v);
+        };
+    }
+
+    public static void ParallelAddToConcurrent(int numberAddItems, string[] k, int[] v)
+    {
+        ConcurrentDictionary<string, int> cd = new();
+
+        Parallel.For(0, numberAddItems, x =>
+        {
+            SomeLongRunningMethod();
+            cd.AddOrUpdate(k[x], v[x], (k, v) => v);
+        });
+    }
+
+    public static void SomeLongRunningMethod()
+    {
+        var a = DateTime.Now.Millisecond;
+        var x = double.MaxValue;
+        x = Math.Sqrt(x);
+        x = Math.Sqrt(a);
+        var b = a + DateTime.Now.Millisecond;
+        x = Math.Sqrt(b);
+        x = Math.Round(Math.Sqrt(a) * Math.Sqrt(b));
+        a = (int)(Math.Log(x) % Math.Log10(x));
+        b = (int)(Math.Log10(x) / Math.Sqrt(x));
+        a = (int)(Math.Sqrt(x));
+        b = (int)(Math.Log(x));
+        x *= x;
+        x /= Math.Log(b);
+        x -= x;
+        x %= Math.Log10(a);
+        x += x;
+        a = 0;
+        b = 0;
+        x = 0;
+    }
+}
