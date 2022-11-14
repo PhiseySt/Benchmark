4d30352cb1fbd894ec2be6d5711ccdc17a4a9f6d
PhiseySt <phstas2016@rambler.ru>
2022-11-14 10:28:11 +0300

    Add files via upload

---
+++ StructVsClassPassAsParam/Benchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace StructVsClassPassAsParam;
+
+
+[MemoryDiagnoser]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn, MeanColumn, MedianColumn]
+[HideColumns("Gen0", "Gen1", "Gen2")]
+public class Benchmark
+{
+    private const int NumIteration = 10000;
+
+    [Benchmark]
+    public void PassStruct() => Methods.PassStruct(NumIteration);
+
+    [Benchmark]
+    public void PassClass() => Methods.PassClass(NumIteration);
+}
---
+++ StructVsClassPassAsParam/Methods.cs
@@ -0,0 +1,57 @@
+﻿namespace StructVsClassPassAsParam;
+
+public static class Methods
+{
+    
+    public static void PassClass(int numIteration)
+    {
+        for (var i = 0; i < numIteration; i++)
+        {
+            var sc = new SomeClass { Val1 = 1000, Val2 = 100000, Val3 = -1000 };
+            LongRunningMethodClass(sc, numIteration);
+        }
+    }
+
+    public static void PassStruct(int numIteration)
+    {
+        for (var i = 0; i < numIteration; i++)
+        {
+            var ss = new SomeStruct { Val1 = 1000, Val2 = 100000, Val3 = -1000 };
+            LongRunningMethodStruct(ss, numIteration);
+        }
+    }
+
+    public static void LongRunningMethodClass(SomeClass sc, int numIteration)
+    {
+        var sc2 = sc;
+        var sum = 0;
+        for (var i = 0; i < numIteration; i++)
+        {
+            sum += i;
+        }
+    }
+
+    public static void LongRunningMethodStruct(SomeStruct ss, int numIteration)
+    {
+        var ss2 = ss;
+        var sum = 0;
+        for (var i = 0; i < numIteration; i++)
+        {
+            sum += i;
+        }
+    }
+}
+
+public class SomeClass
+{
+    public int Val1 { get; set; }
+    public int Val2 { get; set; }
+    public int Val3 { get; set; }
+}
+
+public struct SomeStruct
+{
+    public int Val1 { get; set; }
+    public int Val2 { get; set; }
+    public int Val3 { get; set; }
+}
