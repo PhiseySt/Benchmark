1477b64b8dc0d5b56dc2e5ebbae805426ee9b21b
PhiseySt <phstas2016@rambler.ru>
2023-06-23 15:22:25 +0300

    Add benchmark static random vs random number gener

---
+++ RandomVsRandomNumberGenerator/RandomVsRandomNumberGenerator/ConsoleApp1/Benchmark.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace RandomVsRandomNumberGenerator;
+
+[MemoryDiagnoser]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn, MeanColumn, MedianColumn]
+[HideColumns("Gen0", "Gen1", "Gen2")]
+public class Benchmark
+{
+    private const int NumberGeneration = 100000;
+    private const int MinValue = 0;
+    private const int MaxValue = 1000;
+
+
+    [Benchmark]
+    public void GenerationRandom() => Methods.GenerationRandom(NumberGeneration, MinValue, MaxValue);
+
+    [Benchmark]
+    public void GenerationCryptographicRandom() => Methods.GenerationCryptographicRandom(NumberGeneration, MinValue, MaxValue);
+
+}
---
+++ RandomVsRandomNumberGenerator/RandomVsRandomNumberGenerator/ConsoleApp1/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace RandomVsRandomNumberGenerator;
+
+public class Methods
+{
+    public static void GenerationRandom(int numberGeneration, int minValue, int maxValue)
+    {
+        for (var i = 0; i < numberGeneration; i++)
+        {
+           var val = Random.Shared.Next(minValue, maxValue);
+        }
+    }
+
+    public static void GenerationCryptographicRandom(int numberGeneration, int minValue, int maxValue)
+    {
+        for (var i = 0; i < numberGeneration; i++)
+        {
+            var val = RandomNumberGenerator.GetInt32(minValue, maxValue);
+        }
+    }
+}
