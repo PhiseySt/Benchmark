f269f79889956812071e43973f5c6afd0c4c46f3
PhiseySt <phstas2016@rambler.ru>
2023-12-07 17:27:03 +0300

    Add stopwatchfrequency benchmark

---
+++ StopWatchFrequency/StopWatchFrequency/Benchmark.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace StopWatchFrequency;
+
+[MemoryDiagnoser]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn, MeanColumn, MedianColumn]
+[HideColumns("Gen0", "Gen1", "Gen2")]
+public class Benchmark
+{
+
+    [Benchmark]
+    public void StopwatchLatency() => Methods.StopwatchLatency();
+
+    [Benchmark]
+    public void StopwatchResolution() => Methods.StopwatchResolution();
+
+}
---
+++ StopWatchFrequency/StopWatchFrequency/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace StopWatchFrequency;
+
+public class Methods
+{
+    public static long StopwatchLatency() => Stopwatch.GetTimestamp();
+
+
+    public static long StopwatchResolution()
+    {
+        long lastTimestamp = Stopwatch.GetTimestamp();
+        while (Stopwatch.GetTimestamp() == lastTimestamp)
+        {
+        }
+
+        return lastTimestamp;
+    }
+}
